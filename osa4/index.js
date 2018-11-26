3330d9135a60d0dbcdb2791bdeab4b4c8950ad16
Maarit Ryynänen <ryynma@gmail.com>
2018-11-26 23:16:30 +0200

    Tehtävä 4.2

--- osa4/index.js
+++ osa4/index.js
@@ -4,42 +4,19 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
 if ( process.env.NODE_ENV !== 'production' ) {
-        require('dotenv').config()
+  require('dotenv').config()
 }
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
 
 app.use(cors())
 app.use(bodyParser.json())
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
---
+++ osa4/models/blog.js
@@ -0,0 +1,12 @@
+/* Blogin skeeman määrittelevä moduli */
+
+const mongoose = require('mongoose')
+
+const Blog = mongoose.model('Blog', {
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+module.exports = Blog
